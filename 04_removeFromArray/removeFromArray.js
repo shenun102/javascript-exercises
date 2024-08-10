6b28ea5b4dd80a5116f06126c1d179b071a16cef
shenun102 <124623141+shenun102@users.noreply.github.com>
2024-08-10 13:00:37 +0100

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  // Create new array to be returned
+  const newArr = [];
+  // For each element in arr
+  arr.forEach((arg) => {
+    // if the element is not in args array, then push the current element into a new array
+    if (!args.includes(arg)) newArr.push(arg);
+  });
+  return newArr;
 };
 
 // Do not edit below this line
