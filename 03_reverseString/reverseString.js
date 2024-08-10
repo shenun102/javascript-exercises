10dcbe8126ab0c6aed97960f70f9d78b1d7dc50d
shenun102 <124623141+shenun102@users.noreply.github.com>
2024-08-10 12:32:10 +0100

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+  // Initial solution, very long
+  // let reverseWord = "";
+  // for (let i = word.length - 1; i >= 0; i--) {
+  //   reverseWord += word[i];
+  // }
+  // return reverseWord;
+  return word.split("").reverse().join("");
 };
 
 // Do not edit below this line
