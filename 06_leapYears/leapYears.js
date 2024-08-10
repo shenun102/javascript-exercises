2c09ebf106b1cc46bac58cec8564e91773249d63
shenun102 <124623141+shenun102@users.noreply.github.com>
2024-08-10 15:31:50 +0100

    Complete 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  const diviBy4 = year % 4 === 0;
+  const diviBy100 = year % 100 === 0;
+  const diviBy400 = year % 400 === 0;
 
+  // If false && !(false or false)
+
+  if (diviBy400) {
+    return true;
+  } else if (diviBy100) {
+    return false;
+  } else if (diviBy4) {
+    return true;
+  } else return false;
+
+  // A simpler method would be using short circuit evaluation
+  // return (diviBy400) || (!diviBy100 && diviBy4)
 };
-
 // Do not edit below this line
 module.exports = leapYears;
