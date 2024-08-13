c565af1fb2aeffe4e804672096a4fc8864d2b5ee
shenun102 <124623141+shenun102@users.noreply.github.com>
2024-08-13 17:56:17 +0100

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const alphanumbers = "abcdefghijklmnopqrstuvwxyz123456790";
+  const cleanStr = str
+    .toLowerCase()
+    .split("")
+    .filter((letter) => alphanumbers.includes(letter));
 
+  const finalStr = cleanStr.join("");
+  const reverseStr = cleanStr.reverse().join("");
+
+  if (finalStr === reverseStr) {
+    return true;
+  } else return false;
 };
 
 // Do not edit below this line
