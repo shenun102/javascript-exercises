a902fd28b891a575a1c6cb96d9c8d293eea34796
shenun102 <124623141+shenun102@users.noreply.github.com>
2024-08-14 13:34:05 +0100

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+function getAge(personObj) {
+  const today = new Date();
+  const death = personObj.yearOfDeath;
+  if (!death) return today.getFullYear() - personObj.yearOfBirth;
+  return personObj.yearOfDeath - personObj.yearOfBirth;
+}
 
+const findTheOldest = function (people) {
+  return people.reduce((oldestPerson, currPerson) => {
+    const oldestAge = getAge(oldestPerson);
+    const currPersonAge = getAge(currPerson);
+    // console.log(oldestPerson.name);
+    // console.log(currPersonAge, oldestAge);
+    return currPersonAge < oldestAge ? oldestPerson : currPerson;
+  });
 };
 
 // Do not edit below this line
