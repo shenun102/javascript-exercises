08083f58c2c17254090882c39d760a1a5d7c0527
shenun102 <124623141+shenun102@users.noreply.github.com>
2024-08-14 10:59:49 +0100

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  n = +n;
+  if (n === 0) return 0;
+  if (n < 0) return "OOPS";
+  if (n === 1) return 1;
 
+  let a = 0;
+  let b = 1;
+  for (let i = 2; i <= n; i++) {
+    [a, b] = [b, a + b];
+  }
+  return b;
 };
+// F(n)=F(n−1)+F(n−2)
 
 // Do not edit below this line
 module.exports = fibonacci;
